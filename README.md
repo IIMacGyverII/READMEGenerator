8abf6d65d29db20411d2ea943dfae105a1e8b853
Joshua Christianson <macgyverjunk@yahoo.com>
2022-04-19 17:28:47 -0500

    functional start that has questions and generates a readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Joshua
+## JavaScript
+## undefined
--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+    },
+    {
+    type: 'checkbox',
+    message: 'What languages do you know?',
+    name: 'stack',
+    choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+    type: 'list',
+    message: 'What is your preferred method of communication?',
+    name: 'contact',
+    choices: ['email', 'phone', 'telekinesis'],
+    },
+])
+// .then((data) => {
+//     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+.then((response) =>
+    {
+        let content = `
+## ${response.name}
+## ${response.stack}
+## ${response.communication}`;
+        fs.writeFile('README.md', content, (error) => console.error(error))
+    })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {}
 
 // Function call to initialize app
-init();
+// init();
