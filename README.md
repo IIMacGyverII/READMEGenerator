d2a2b31caa9782f70012318e0987428e069c63f8
Joshua Christianson <macgyverjunk@yahoo.com>
2022-04-19 18:30:41 -0500

    added lots of questions and testing the screenshot

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 
-## Joshua
-## JavaScript
-## undefined
+# Project Title: asdass
+## Description:
+    asd
+## What motivated me:
+    asda
+## Why did I build this project?
+    sdasd
+## What problem does this solve?
+    asda
+## What did you learn?
+    asdadw
+## Installation
+    adqwdawd
+## Usage
+    adwdawda
+## Screenshot
+    https://us.v-cdn.net/6029997/uploads/8FJ1X80MY6JY/image.png
--- index.js
+++ index.js
@@ -6,35 +6,83 @@ inquirer
     .prompt([
     {
     type: 'input',
-    name: 'name',
-    message: 'What is your name?',
+    name: 'projectName',
+    message: 'What is your projects name?',
     },
     {
-    type: 'checkbox',
-    message: 'What languages do you know?',
-    name: 'stack',
-    choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+        type: 'input',
+        message: 'Enter a description for this project:',
+        name: 'description',
+        // choices: ['Choice1', 'Choice2', 'Choice3', 'Choice4'],
     },
     {
-    type: 'list',
-    message: 'What is your preferred method of communication?',
-    name: 'contact',
-    choices: ['email', 'phone', 'telekinesis'],
+    type: 'input',
+    message: 'What was your motivation for this project?',
+    name: 'motivation',
+    
+    },
+    {
+    type: 'input',
+    message: 'Why did you build this project?',
+    name: 'whyDidYou',
+    
+    },
+    {
+        type: 'input',
+        name: 'problemSolved',
+        message: 'What problem does this solve?',
+        
+    },
+    {
+        type: 'input',
+        message: 'What did you learn?',
+        name: 'whatDidYouLearn',
+        
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install this?',
+        
+    },
+    {
+        type: 'input',
+        message: 'How do you use this project?',
+        name: 'usage',
+        
+    },
+    {
+        type: 'input',
+        message: 'Add a link to a screenshot or gif',
+        name: 'screenShot',
+        
     },
 ])
 // .then((data) => {
 //     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+// TODO: Create a function to write README file
 .then((response) =>
     {
         let content = `
-## ${response.name}
-## ${response.stack}
-## ${response.communication}`;
+# Project Title: ${response.projectName}
+## Description:
+    ${response.description}
+## What motivated me:
+    ${response.motivation}
+## Why did I build this project?
+    ${response.whyDidYou}
+## What problem does this solve?
+    ${response.problemSolved}
+## What did you learn?
+    ${response.whatDidYouLearn}
+## Installation
+    ${response.installation}
+## Usage
+    ${response.usage}
+## Screenshot
+    ${response.screenShot}`;
         fs.writeFile('README.md', content, (error) => console.error(error))
     })
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 // function init() {}
